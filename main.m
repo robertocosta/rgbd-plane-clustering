bdd8fc61ce843db9edad387ffea9fb62b0cdb26a
Roberto Costa <robertocosta1993@gmail.com>
2018-01-10 17:51:25 +0100

    .

--- main.m
+++ main.m
@@ -60,7 +60,7 @@ for i=1:size(ordInd,1)
             depth = depths(:,:,ordInd{i,3}{j,2}(k));
             raw_depth =depth;
             %gauss_smoothed = imgaussfilt3(cat(3,im,depth));
-            depth = imgaussfilt(depth);
+            depth = imgaussfilt(raw_depth,0.6);
             %raw_depth = rawDepths(:,:,ordInd{i,3}{j,2}(k));
             lab = labels(:,:,ordInd{i,3}{j,2}(k));
             f = show_im(im,depth,lab);
@@ -73,16 +73,36 @@ for i=1:size(ordInd,1)
             [p, n, conf] = compute_local_planes(x,y,z);
             %nn_approach = [x(:),y(:),z(:),reshape(p,N,4),double(reshape(im(45:471, 41:601, :),N,3))/255];
             %kmeans_input = [reshape(p,N,4),double(reshape(rgb2lab(im(45:471, 41:601, :)),N,3))/255];
-            kmeans_input = [reshape(n,N,3),double(reshape(rgb2lab(im(45:471, 41:601, :)),N,3))/255];
-            [idx,C,sumd,D] = kmeans(kmeans_input,6);
-            plot3D_labeled([x(:),y(:),z(:)],idx);
+            
+            % normali
             kmeans_input = reshape(n,N,3);
-            [idx2,C2,sumd2,D2] = kmeans(kmeans_input,6);
+            [idx1,C1,~,D1] = kmeans(kmeans_input,6);
+            plot3D_labeled([x(:),y(:),z(:)],idx1);
+            title("K-means Normals");
+            [class_ord1, max_ind1] = sort_by_variance(D1,idx1,0.8);
+            
+            % piani
+            kmeans_input = reshape(p,N,4);
+            [idx2,C2,~,D2] = kmeans(kmeans_input,6);
             plot3D_labeled([x(:),y(:),z(:)],idx2);
+            title("K-means Planes");
+            [class_ord2, max_ind2] = sort_by_variance(D1,idx1,0.8);
+            
+            % piani, colore
             kmeans_input = [reshape(p,N,4),double(reshape(rgb2lab(im(45:471, 41:601, :)),N,3))/255];
-            [idx3,C3,sumd3,D3] = kmeans(kmeans_input,6);
-            plot3D_labeled([x(:),y(:),z(:)],idx);
-
+            [idx3,C3,~,D3] = kmeans(kmeans_input,6);
+            plot3D_labeled([x(:),y(:),z(:)],idx3);
+            title("K-means Planes + color");
+            [class_ord3, max_ind3] = sort_by_variance(D1,idx1,0.8);
+            
+            % normali, colore
+            kmeans_input = [reshape(n,N,3),double(reshape(rgb2lab(im(45:471, 41:601, :)),N,3))/255];
+            [idx4,C4,~,D4] = kmeans(kmeans_input,6);
+            plot3D_labeled([x(:),y(:),z(:)],idx4);
+            title("K-means Normals + color");
+            [class_ord4, max_ind4] = sort_by_variance(D1,idx1,0.8);
+           
+            
             [planes, plane_idx] = xyz2planes_ransac(x,y,z,...
                 reshape(n,N,3),abs(conf)>0.5);
             lab1 = lab(45:471, 41:601);
