049c7b9158dfe999c7c2edd42cca76e19dc12ac7
Roberto Costa <robertocosta1993@gmail.com>
2018-01-18 18:24:28 +0100

    removing old files

--- lib/old/find_most_frequent.m
+++
@@ -1,7 +0,0 @@
-function n = find_most_frequent(tab,edges)
-%FIND_MOST_FREQUENT Summary of this function goes here
-%   Detailed explanation goes here
-freq = histcounts(tab(tab>0),edges);
-n = find(freq==max(freq))+1;
-end
-
