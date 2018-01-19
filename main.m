d07835bb58e8689bceb40fd678f118e3dd9e60f4
Roberto Costa <robertocosta1993@gmail.com>
2018-01-19 14:12:36 +0100

    planes recognition

--- main.m
+++ main.m
@@ -56,22 +56,13 @@ for i=1:size(ordInd,1)
     % row: each row is composed by [scene, list of indexes of that scene]
     for j = 1:size(ordInd{i,3},1)
         for k=1:length(ordInd{i,3}{j,2})
+            min_pts_per_plane = 5000;
+            var_gauss_filt_depth = 0.1;
+            
             im = images(:,:,:,ordInd{i,3}{j,2}(k));
             depth = depths(:,:,ordInd{i,3}{j,2}(k));
             raw_depth =depth;
-            
-            %gauss_smoothed = imgaussfilt3(cat(3,im,depth));
-            depth = imgaussfilt(raw_depth,0.1);
-%             depth2 = imgaussfilt(raw_depth,0.1);
-%             depth_gradient = single(zeros(size(depth2,1)-1,size(depth2,2)-1,2));
-%             for ii=1:size(depth_gradient,1)
-%                 for jj=1:size(depth_gradient,2)
-%                     depth_gradient(ii,jj,1) = depth2(ii+1,jj)-depth2(ii,jj);
-%                     depth_gradient(ii,jj,2) = depth2(ii,jj+1)-depth2(ii,jj);
-%                 end
-%             end
-%             depth_gradient = depth_gradient(45:471, 41:601, :);
-            %raw_depth = rawDepths(:,:,ordInd{i,3}{j,2}(k));
+            depth = imgaussfilt(raw_depth,var_gauss_filt_depth);
             lab = labels(:,:,ordInd{i,3}{j,2}(k));
             f = show_im(im,depth,lab);
             xyz = rgb_p2rgb_w(depth);
@@ -81,9 +72,11 @@ for i=1:size(ordInd,1)
             y = xyz(45:471, 41:601, 2);
             z = xyz(45:471, 41:601, 3);
             xyz = [x(:),y(:),z(:)];
+            
             [p, n, conf] = compute_local_planes(x,y,z);
             [planes, planes_idx] = xyz2planes_ransac(x,y,z,...
-                reshape(n,N,3),abs(conf)>0.5);
+                reshape(n,N,3),abs(conf)>0.5,min_pts_per_plane);
+            
             n_lab = length(planes_idx);
             
             lab0 = lab(45:471, 41:601);
@@ -92,30 +85,43 @@ for i=1:size(ordInd,1)
             title('labeled');
             plot3D_labeled([x(:),y(:),z(:)],idx);
             title("xyz2planes-ransac");
-            inliers = cell(2,1);
+            
             expanded_region = cell(2,1);
-            inliers_planes = zeros(size(planes));
-            vars = zeros(n_lab,1);
-            inliers_percent = zeros(n_lab,1);
+            lab1 = uint8(zeros(N,1));
             for ii=1:n_lab
                 planes(ii,:) = planes(ii,:)/norm(planes(i,:));
-                inliers{ii} = find_inliers(xyz,find(idx==ii));
-                expanded_region{ii} = geo_region_growing(xyz,inliers{ii});
-                inliers_percent(ii) = length(inliers{ii})/...
-                    double(length(planes_idx{ii}))*100;
-                disp(strcat(num2str(inliers_percent(ii)),' % inliers'));
-                if inliers_percent(ii)>70
-                    lab1 = uint8(zeros(N,1));
-                    lab2 = uint8(zeros(N,1));
-                    lab1(idx==ii) = 1;
-                    lab2(inliers{ii}) = 2;
-                    plot3D_labeled([x(:),y(:),z(:)],lab1);
-                    title(strcat('all points - ',num2str(ii)));
-                    plot3D_labeled([x(:),y(:),z(:)],lab2);
-                    title(strcat('inliers only - ',num2str(ii)));
+%                 plane_found = false;
+%                 n_points = length(planes_idx{ii});
+%                 while (~plane_found)
+%                     expanded_region{ii} = region_growing(xyz,planes(ii,:),...
+%                         planes_idx{ii}(randi(n_points,round(n_points/6),1)),0.08);
+%                     plane_found = length(expanded_region{ii})>n_points/2;
+%                 end
+                expanded_region{ii} = point_belongs_to_plane(xyz,...
+                    planes(ii,:),0.08);
+                if (length(planes_idx{ii})>min_pts_per_plane)
+                    lab1(setdiff(find(expanded_region{ii}==1),...
+                        find(lab1>0))) = ii;
                 end
-                [inliers_planes(ii,:), vars(ii)] = find_plane(xyz(inliers{ii},:));
+
             end
+            planes2 = zeros(n_lab,4);
+            var_planes = zeros(n_lab,1);
+            for ii=1:n_lab
+                [planes2(ii,:), var_planes(ii)]= find_plane(xyz(lab1==ii,:));
+            end
+            [~, ordered] = sort(var_planes);
+            for ii=1:n_lab
+                %waitforbuttonpress;
+                %close all;
+                lab2 = zeros(N,1);
+                lab2(lab1==ordered(ii)) = 1;
+                if (length(planes_idx{ordered(ii)})>min_pts_per_plane)
+                    plot3D_labeled([x(:),y(:),z(:)],lab2);
+                    disp(num2str(ordered(ii)));
+                end
+            end
+            plot3D_labeled([x(:),y(:),z(:)],lab1);
         end
     end
 end
