3556a55b9646eac630fa96a048b3e57d004c1873
Roberto Costa <robertocosta1993@gmail.com>
2018-01-22 01:52:58 +0100

    normals computation

--- py/real_time_plane_segm_rgbd.py
+++ py/real_time_plane_segm_rgbd.py
@@ -13,18 +13,19 @@ class plane_clustering:
         self.smoothed = kwargs.get('smoothed', False)
         self.sigm = kwargs.get('sigma', 1)
         self.n = 0
-        if not self.__load_mat():
+        if not self.__load_mat(): # self.dataset_dict, self.n
             print('Error loading \'mat\' dataset')
             exit()
         self.var_names = ('accelData', 'depths', 'rawDepths', 'images', 'instances',
                           'labels', 'names', 'namesToIds', 'sceneTypes', 'scenes')
         self.current_index = -1
+        self.normals = []
         self.next()
 
     def next(self):
         if self.current_index+1 >= self.n:
             print('Finished')
-            exit()
+            return False
         self.current_index = self.current_index+1
         self.image = self.dataset_dict['images'][:,:,:,self.current_index]
         shap = self.image.shape
@@ -34,13 +35,16 @@ class plane_clustering:
         if self.smoothed:
             self.depth = ndimage.gaussian_filter(self.depth,sigma=(self.sigm,self.sigm),order=0)
         self.label = self.dataset_dict['labels'][:,:,self.current_index]
-        self.point_cloud = np.zeros((3, self.H, self.W))
-        self.__depth2xyz()
-        print('data loaded successfully')
-        self.compute_normals()
+        # from depth to [x,y,z] coordinates
+        self.point_cloud = self.__depth2xyz()
+        print('Image '+str(self.current_index+1)+' of '+str(self.n)+' loaded successfully')
+        # computing average normals
+        self.avg_win_long = 2  # half of the greatest dimension of the avg window
+        self.avg_win_short = 1 # half of the smaller dimension of the avg window
+        self.normals.append(self.compute_normals())
+        return True
 
     def compute_normals(self):
-        #self.point_cloud
         H = self.H
         W = self.W
         tang_x = np.zeros((3,H,W))
@@ -69,6 +73,35 @@ class plane_clustering:
                 integral_yz[i,j] = integral_yz[i-1,j]+integral_yz[i,j-1]-integral_yz[i-1,j-1]+tang_y[2,i,j]
         print('integral image of tangents computed')
         # vectorial product between the 2 tangents to get the normal
+        normals = np.zeros((3,H,W))
+        w1 = self.avg_win_long # half of the greatest dimension of the averaging window
+        w2 = self.avg_win_short # half of the smaller dimension of the averaging window
+        for i in range(w1,H-w1):
+            for j in range(w1,W-w1):
+                tan_xx_l = integral_xx[i+w1,j]-integral_xx[i+w1,j-w2]-integral_xx[i-w1,j]+integral_xx[i-w1,j-w2]
+                tan_xx_r = integral_xx[i+w1,j+w2]-integral_xx[i+w1,j]-integral_xx[i-w1,j+w2]+integral_xx[i-w1,j]
+                tan_xy_l = integral_xy[i+w1,j]-integral_xy[i+w1,j-w2]-integral_xy[i-w1,j]+integral_xy[i-w1,j-w2]
+                tan_xy_r = integral_xy[i+w1,j+w2]-integral_xy[i+w1,j]-integral_xy[i-w1,j+w2]+integral_xy[i-w1,j]
+                tan_xz_l = integral_xz[i+w1,j]-integral_xz[i+w1,j-w2]-integral_xz[i-w1,j]+integral_xz[i-w1,j-w2]
+                tan_xz_r = integral_xz[i+w1,j+w2]-integral_xz[i+w1,j]-integral_xz[i-w1,j+w2]+integral_xz[i-w1,j]
+                tan_yx_u = integral_yx[i,j+w1]-integral_yx[i,j-w1]-integral_yx[i-w2,j+w1]+integral_yx[i-w2,j-w1]
+                tan_yx_d = integral_yx[i+w2,j+w1]-integral_yx[i,j+w1]-integral_yx[i+w2,j-w1]+integral_yx[i,j-w1]
+                tan_yy_u = integral_yy[i,j+w1]-integral_yy[i,j-w1]-integral_yy[i-w2,j+w1]+integral_yy[i-w2,j-w1]
+                tan_yy_d = integral_yy[i+w2,j+w1]-integral_yy[i,j+w1]-integral_yy[i+w2,j-w1]+integral_yy[i,j-w1]
+                tan_yz_u = integral_yz[i,j+w1]-integral_yz[i,j-w1]-integral_yz[i-w2,j+w1]+integral_yz[i-w2,j-w1]
+                tan_yz_d = integral_yz[i+w2,j+w1]-integral_yz[i,j+w1]-integral_yz[i+w2,j-w1]+integral_yz[i,j-w1]
+                tan_xx = (tan_xx_r-tan_xx_l)/((2*w1+1)*(2*w2+1))
+                tan_xy = (tan_xy_r-tan_xy_l)/((2*w1+1)*(2*w2+1))
+                tan_xz = (tan_xz_r-tan_xz_l)/((2*w1+1)*(2*w2+1))
+                tan_yx = (tan_yx_d-tan_yx_u)/((2*w1+1)*(2*w2+1))
+                tan_yy = (tan_yy_d-tan_yy_u)/((2*w1+1)*(2*w2+1))
+                tan_yz = (tan_yz_d-tan_yz_u)/((2*w1+1)*(2*w2+1))
+                tan_x = np.array([tan_xx,tan_xy,tan_xz])
+                tan_y = np.array([tan_yx,tan_yy,tan_yz])
+                norm = np.cross(tan_x,tan_y)
+                normals[:,i,j] = norm
+        print('normals computed')
+        return normals
 
     def __load_mat(self):
         try:
@@ -80,24 +113,26 @@ class plane_clustering:
         return False
 
     def __depth2xyz(self):
+        point_cloud = np.zeros((3, self.H, self.W))
         depth = self.depth
         fx_rgb = 5.1885790117450188e+02
         fy_rgb = 5.1946961112127485e+02
         cx_rgb = 3.2558244941119034e+02
         cy_rgb = 2.5373616633400465e+02
         [xx, yy] = np.meshgrid(range(1,self.W+1),range(1,self.H+1))
-        self.point_cloud[0,:,:] = (xx - cx_rgb) * depth / fx_rgb
+        point_cloud[0,:,:] = (xx - cx_rgb) * depth / fx_rgb
         y = (yy - cy_rgb) * depth / fy_rgb
         y = -y+np.ndarray.max(y)
-        self.point_cloud[1,:,:] = depth
-        self.point_cloud[2,:,:] = y
+        point_cloud[1,:,:] = depth
+        point_cloud[2,:,:] = y
+        return point_cloud
 
 
 
 if __name__ == "__main__":
     #pc = plane_clustering(mat_path='../../3DFinalProj/mat/reduced_dataset.mat')
     pc = plane_clustering(mat_path='../reduced_reduced_dataset.mat',smoothed=True, sigma=0.8)
-    
+    while (pc.next()): pass
     
     #print(pc.point_cloud)
     #plt.imshow(pc.image, interpolation='nearest')
